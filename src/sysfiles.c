5ac0273bfee94a25d17b41172022406b85f1a472
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2017-03-14 15:40:54 +0000

    Fix an #ifdef bug

--- src/sysfiles.c
+++ src/sysfiles.c
@@ -1298,7 +1298,9 @@ void getwindowsize( void )
             if (CO <= 0)
                 CO = win.ws_col;
         }
+#if !defined(LIBGAP)
         (void) signal(SIGWINCH, syWindowChangeIntr);
+#endif
     }
 #endif /* TIOCGWINSZ */
 
